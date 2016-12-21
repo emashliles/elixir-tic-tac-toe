a0b0752e6633c772335d65cd8fee9e5ef8c2c2ce
EA <emashliles@gmail.com>
2016-12-21 11:09:26 +0000

    Can now replace any board tile

--- lib/tictactoe.ex
+++ lib/tictactoe.ex
@@ -2,19 +2,27 @@ defmodule TicTacToe do
   use Mix.Task
 
   def start() do
-    play()
+    IO.puts(play())
   end
 
   def play() do
-    '123\n456\n789'
+    "123\n456\n789"
   end
+#
+#  def play(5) do
+#    '123\n4X6\n789'
+#  end
+#
+#  def play(9) do
+#    '123\n456\n78X'
+#  end
+#
+#  def play(1) do
+#    'X23\n456\n789'
+#  end
 
-  def play(5) do
-    '123\n4X6\n789'
-  end
-
-  def play(9) do
-    '123\n456\n78X'
+  def play(x) do
+    String.replace("123\n456\n789",Integer.to_string(x),"X")
   end
 
 end
--- test/tictactoe_test.exs
+++ test/tictactoe_test.exs
@@ -3,15 +3,19 @@ defmodule TicTacToeTest do
   doctest TicTacToe
 
   test "it can print the board" do
-    assert TicTacToe.play() == '123\n456\n789'
+    assert TicTacToe.play() == "123\n456\n789"
   end
 
   test "player can select tile 5" do
-    assert TicTacToe.play(5) == '123\n4X6\n789'
+    assert TicTacToe.play(5) == "123\n4X6\n789"
   end
 
   test "player can select tile 9" do
-    assert TicTacToe.play(9) == '123\n456\n78X'
+    assert TicTacToe.play(9) == "123\n456\n78X"
+  end
+
+  test "player can select tile 1" do
+    assert TicTacToe.play(1) == "X23\n456\n789"
   end
 
 end
