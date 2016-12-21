5e7095584573ec74737f0317e99401a144e4d4f4
EA <emashliles@gmail.com>
2016-12-21 10:03:49 +0000

    Player can now select tile 9

--- lib/tictactoe.ex
+++ lib/tictactoe.ex
@@ -12,4 +12,9 @@ defmodule TicTacToe do
   def play(5) do
     '123\n4X6\n789'
   end
+
+  def play(9) do
+    '123\n456\n78X'
+  end
+
 end
--- test/tictactoe_test.exs
+++ test/tictactoe_test.exs
@@ -6,8 +6,12 @@ defmodule TicTacToeTest do
     assert TicTacToe.play() == '123\n456\n789'
   end
 
-  test "player can select a tile" do
+  test "player can select tile 5" do
     assert TicTacToe.play(5) == '123\n4X6\n789'
+  end
+
+  test "player can select tile 9" do
+    assert TicTacToe.play(9) == '123\n456\n78X'
   end
 
 end
