bb8f1541cfae00516cc377ac41ceca915ad33d8c
EA <emashliles@gmail.com>
2016-12-29 13:34:13 +0000

    Separated getting tile selection

--- lib/tictactoe_ui.ex
+++ lib/tictactoe_ui.ex
@@ -1,17 +1,18 @@
 defmodule TicTacToe.UI do
 
   def start_game() do
-    game_status =  TicTacToe.make_move()
-    elem(game_status, 1)
-    |> insert_new_lines_into_board()
-    |> IO.puts 
-    game_status
+    TicTacToe.make_move()
+    |> decide_turn_type()
   end
   
   def get_move(game_status) do
-     IO.gets("Please enter a tile selection:")
-     |> String.trim()
+    get_tile_selection(game_status)
      |> make_and_print_move(game_status)
+  end
+
+  def get_tile_selection(game_status) do
+    IO.gets("Please enter a tile selection:")
+    |> String.trim()
   end
 
   def make_and_print_move(selected_tile, game_status) do 
