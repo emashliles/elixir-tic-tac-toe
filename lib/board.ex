b9c0fe2182d31a69f62dfab0d1e3ef8163356e24
EA <emashliles@gmail.com>
2017-01-11 10:02:25 +0000

    Refactoring: Destructured game status in function parameters

--- lib/board.ex
+++ lib/board.ex
@@ -4,20 +4,18 @@ defmodule Board do
     {:x, [["1","2","3"],["4","5","6"],["7","8","9"]], :first_turn}
   end
 
-  def place_marker(selected_tile, game_status) do
-    player_symbol = elem(game_status, 0)
-    create_next_game_status(elem(game_status, 1), player_symbol, selected_tile)
+  def place_marker(selected_tile, {player_symbol, board, _}) do
+    create_next_game_status(board, player_symbol, selected_tile)
   end
 
   def check_tile_not_taken(board, selected_tile) do
     selection_value = get_value_of_selected_tile(zero_index_selection(selected_tile), board)
-    
+
    if Enum.member?(get_all_marker_values,selection_value) do
      :tile_already_selected
    else
      :continue
    end
-
   end
 
   defp create_next_game_status(board, player_symbol, selected_tile) do
@@ -30,7 +28,7 @@ defmodule Board do
     marker_string = get_marker_symbol(player_symbol)
     index_selection = zero_index_selection(selected_tile)
     row_size = length(board)
-    
+
     do_replace_marker_in_board(board, index_selection, marker_string, row_size)
   end
 
@@ -44,7 +42,7 @@ defmodule Board do
     List.flatten(board)
     |> Enum.at(selected_tile)
   end
-  
+
   defp get_marker_symbol(player_symbol), do: Markers.get_player_marker(player_symbol)
 
   defp get_all_marker_values(), do: Markers.get_all_markers()
